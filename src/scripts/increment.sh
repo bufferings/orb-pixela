f682294427bbc91a7238e70264cb12ce6e38af0e
Mitsuyuki Shiiba <bufferings@gmail.com>
2022-03-21 03:23:02 +0900

    Try dev

--- src/scripts/increment.sh
+++ src/scripts/increment.sh
@@ -1,5 +1,10 @@
+PARAM_TOKEN=$(eval "echo ${PARAM_TOKEN}")
+PARAM_USERNAME=$(eval "echo ${PARAM_USERNAME}")
+PARAM_GRAPH_ID=$(eval "echo ${PARAM_GRAPH_ID}")
+
 Increment() {
-    curl -X PUT "https://pixe.la/v1/users/<<parameters.username>>/graphs/<<parameters.graph-id>>/increment" -H "X-USER-TOKEN:<<parameters.token>>" -H 'Content-Length:0'
+    curl -X PUT -H "X-USER-TOKEN:${PARAM_TOKEN}" -H 'Content-Length:0' \
+      "https://pixe.la/v1/users/${PARAM_USERNAME}/graphs/${PARAM_GRAPH_ID}/increment"
 }
 
 # Will not run if sourced for bats-core tests.
