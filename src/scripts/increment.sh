cb2463495306b5ef7fa8fbc026e0ba954cdaf25a
Mitsuyuki Shiiba <bufferings@gmail.com>
2022-03-21 10:19:38 +0900

    Check response

--- src/scripts/increment.sh
+++ src/scripts/increment.sh
@@ -3,8 +3,14 @@ PARAM_USERNAME=$(eval "echo ${PARAM_USERNAME}")
 PARAM_GRAPH_ID=$(eval "echo ${PARAM_GRAPH_ID}")
 
 Increment() {
-    curl -X PUT -H "X-USER-TOKEN:${PARAM_TOKEN}" -H 'Content-Length:0' \
-      "https://pixe.la/v1/users/${PARAM_USERNAME}/graphs/${PARAM_GRAPH_ID}/increment"
+    http_response=$(curl -s -X PUT -H "X-USER-TOKEN:${PARAM_TOKEN}" -H 'Content-Length:0' \
+      "https://pixe.la/v1/users/${PARAM_USERNAME}/graphs/${PARAM_GRAPH_ID}/increment" \
+      | jq '.isSuccess')
+    if [ $http_response != "200" ]; then
+        return 22
+    else
+        return 0
+    fi
 }
 
 # Will not run if sourced for bats-core tests.
